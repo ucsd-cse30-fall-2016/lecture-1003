4a516fe687dc6c12a5a338bf7ee0f84e4d50ee31
Diba Mirza <dimirza@eng.ucsd.edu>
2016-10-03 13:48:44 -0700

    lectire 10/03

---
+++ hello.s
@@ -0,0 +1,37 @@
+	.arch armv6
+	.eabi_attribute 27, 3
+	.eabi_attribute 28, 1
+	.fpu vfp
+	.eabi_attribute 20, 1
+	.eabi_attribute 21, 1
+	.eabi_attribute 23, 3
+	.eabi_attribute 24, 1
+	.eabi_attribute 25, 1
+	.eabi_attribute 26, 2
+	.eabi_attribute 30, 6
+	.eabi_attribute 34, 1
+	.eabi_attribute 18, 4
+	.file	"hello.c"
+	.section	.rodata
+	.align	2
+.LC0:
+	.ascii	"Hello world\000"
+	.text
+	.align	2
+	.global	main
+	.type	main, %function
+main:
+	@ args = 0, pretend = 0, frame = 0
+	@ frame_needed = 1, uses_anonymous_args = 0
+	stmfd	sp!, {fp, lr}
+	add	fp, sp, #4
+	ldr	r0, .L2
+	bl	puts
+	ldmfd	sp!, {fp, pc}
+.L3:
+	.align	2
+.L2:
+	.word	.LC0
+	.size	main, .-main
+	.ident	"GCC: (Raspbian 4.9.2-10) 4.9.2"
+	.section	.note.GNU-stack,"",%progbits
